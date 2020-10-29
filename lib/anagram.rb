0a364ff198bf9123c377a6a74fb683b177e720b4
claire <clairebontempo@gmail.com>
2020-10-29 12:34:17 -0700

    commit tree 74d3b67c3c360c262dcfad9c41120ebe89d6c554

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,28 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(word_array) 
+        #iterate over array of words that #match takes as argument
+        #compare each word of array to the @word
+        #determine if they are composed of the same letters
+        #split word into array of letters
+        #compare arrays by .sort and ==
+        match_array = []
+        word_array.collect do |anagram_hunt| 
+            if anagram_hunt.split("").sort == @word.split("").sort
+        match_array << anagram_hunt 
+        end
+        end
+       match_array
+    end 
+
+end
+
+#word = "listen"
+#array(%w(enlists google inlets banana))
